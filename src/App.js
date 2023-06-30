111fe893d12e69ba51af7baefc556eaeb6f921ee
Daniel Mooney <daniel.mooney180@gmail.com>
2023-07-01 00:30:02 +1000

    bad git management, forgot what I did

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import "./app.css"
+
+import drc1 from "./img/drc_1.jpg"
 
 function App() {
-  return null
+  
+
+  return (
+    <div className='content-body'>
+
+    </div>
+  )
 }
 
 export default App;
