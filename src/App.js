7ff64bab6265f31293ebec36ae27f2a4ce63d36e
Daniel Mooney <daniel.mooney180@gmail.com>
2023-07-01 19:54:12 +1000

    completed basic dropdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import Dropdown from './Components/Dropdown'
-
 import "./app.css"
 
 import drc1 from "./img/drc_1.jpg"
 
 function App() {
-  
-
   return (
     <div className='content-body'>
-      <Dropdown name={"Drop down"}>
-
+      <Dropdown name='Name'>
+        <p>Test</p>
+        <p>More text</p>
+        <img src={drc1} style={{height: '40vh'}}/>
       </Dropdown>
     </div>
   )
