73b687a59d8a0f74db62511c3ffb0ad210d34e80
Daniel Mooney <daniel.mooney180@gmail.com>
2023-07-02 22:39:51 +1000

    achevied basic functionality. Form does not submit

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import "./contactForm.css"
 
 export default function ContactForm() {
   return (
-    <div>ContactForm</div>
+    <>
+        <form>
+            <div className='form-container'>
+                <div className='contact-details-container'>
+                    <input className='contact-name' type='text' placeholder='Name'/>
+                    <input className='contact-email' type='email' placeholder='Email'/>
+                </div>
+                {/* <input className='contact-msg' type='text' placeholder='Message'/> */}
+                <textarea className='contact-msg' placeholder='Message' rows='8'/>
+            </div>
+        </form>
+    </>
   )
 }
