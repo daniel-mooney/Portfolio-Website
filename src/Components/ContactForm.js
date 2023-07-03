8d76671aadb06abe8cf46616bcb642819992b53e
Daniel Mooney <daniel.mooney180@gmail.com>
2023-07-03 14:05:16 +1000

    email functionality achieved using emailjs

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,4 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
+
 import "./contactForm.css"
 
 const NAME_ID = "form-name";
@@ -6,17 +8,21 @@ const EMAIL_ID = "form-email";
 const MSG_ID = "form-msg";
 
 export default function ContactForm() {
-
+    const form = useRef(null);
     const nameField = useRef(null);
     const emailField = useRef(null);
     const msgField = useRef(null);
     
-    function submitForm(e) {
+    function emailForm(e) {
         e.preventDefault()
         console.log(nameField.current.value);
         console.log(emailField.current.value);
         console.log(msgField.current.value);
+        
+        emailjs.sendForm('service_empzo39', 'PFW_contact_form', form.current, 'SVWBkRhHG3q9jAL-l');
+            // .then()
 
+        alert("Message Sent");
         clearForm();
     }
 
@@ -28,13 +34,13 @@ export default function ContactForm() {
 
     return (
     <>
-        <form onSubmit={submitForm}>
+        <form id='contact-form' ref={form} onSubmit={emailForm}>
             <div className='form-container'>
                 <div className='contact-details-container'>
-                    <input ref={nameField} className='contact-name' type='text' placeholder='Name'/>
-                    <input ref={emailField} className='contact-email' type='email' placeholder='Email'/>
+                    <input ref={nameField} type='text' placeholder='Name' name='name' size={23} required/>
+                    <input ref={emailField}  type='email' placeholder='Email' name='email' size={42} required/>
                 </div>
-                <textarea ref={msgField} className='contact-msg' placeholder='Message' rows='8'/>
+                <textarea ref={msgField} className='contact-msg' placeholder='Message' name='msg' rows='8' required/>
                 <div className='contact-button-container'>
                     <button className='contact-button'>Send</button>
                 </div>
